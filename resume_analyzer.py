f005122e1e571d3a803e9b80fed43a0733948deb
Geetika95-Tyagi <geetikatyagi9415@gmail.com>
2026-01-03 13:57:58 +0530

    Add files via upload

--- resume_analyzer.py
+++ resume_analyzer.py
@@ -1,1 +1,17 @@
-print("AI Resume Analyzer Project Started")
+print("Welcome to AI Resume Analyzer")
+
+resume = input("Paste your resume text here: ").lower()
+
+job_skills = ["python", "java", "machine learning", "cloud", "sql"]
+
+matched_skills = []
+
+for skill in job_skills:
+    if skill in resume:
+        matched_skills.append(skill)
+
+score = (len(matched_skills) / len(job_skills)) * 100
+
+print("\nMatched Skills:", matched_skills)
+print("Resume Score:", score, "%")
+
